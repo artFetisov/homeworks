7c695865fcf6af372e1634f50643f94b2d5c33db
artFetisov <fetishfestoff@mail.ru>
2022-06-29 00:02:59 +0300

    Add homework 2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import {AffairType} from "./HW2";
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+
+    affair: AffairType // need to fix any
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
+    }
 
     return (
         <div>
-            // show some text
+            <span>{'skmkmdkdkdm'}</span>
 
             <button onClick={deleteCallback}>X</button>
         </div>
